53838d158c64202124cabc5e2c9725dc2b215bcd
Kirill <s.kirill10@gmail.com>
2024-03-05 21:46:53 +1000

    05\03\24

--- Store server/Store_server/Products/admin.py
+++ Store server/Store_server/Products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from Products.models import Product, Product_Category
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Product_Category)
--- Store server/Store_server/Products/views.py
+++ Store server/Store_server/Products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
+from Products.models import Product_Category, Product
+
 def index(request):
     context = {
         'title':'Title`s store',
@@ -13,31 +15,7 @@ def products(request):
     context = {
         'title':'Store - Каталог товаров',
         'user_name':'qwerty',
-        'products_item': [
-            {
-                'name' : 'Худи черного цвета с монограммами adidas Originals',
-                'desc' : 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.', 
-                'image' : "/static/vendor/img/products/Adidas-hoodie.png",
-                'price' : '6 090,00'
-            },
-            {
-                'name' : 'Синяя куртка The North Face',
-                'desc' : 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.', 
-                'image' : "/static/vendor/img/products/Blue-jacket-The-North-Face.png",
-                'price' : '23 725,00',
-            },
-             {
-                'name' : 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'desc' : 'Материал с плюшевой текстурой. Удобный и мягкий..', 
-                'image' : "/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png",
-                'price' : '3 390,00 руб.',
-            },
-             {
-                'name' : 'Черный рюкзак Nike Heritage',
-                'desc' : 'Плотная ткань. Легкий материал.', 
-                'image' : "/static/vendor/img/products/Black-Nike-Heritage-backpack.png",
-                'price' : '2 340,00',
-            }
-        ]
+        'products_item': Product.objects.all(),
+        'category' : Product_Category.objects.all(), 
     }
     return render(request, 'Product\products.html', context)
