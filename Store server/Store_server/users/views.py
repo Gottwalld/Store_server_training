85d51fc4e7acced026ce72b32cbbdb16086223e8
Kirill <s.kirill10@gmail.com>
2024-03-12 22:08:36 +1000

    12/03

--- Store server/Store_server/users/views.py
+++ Store server/Store_server/users/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username = username, password = password)
+        if user:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        form = UserLoginForm()
     context = {'form': UserLoginForm()}
     return render(request, 'Users/login.html', context)
 
